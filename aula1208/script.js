610863e93ddf03a5b389fe793746a08f3ee2317a
mariaceci573 <mariacecilia.oliveira@estudante.iftm.edu.br>
2025-08-12 17:35:07 -0300

    Add files via upload

---
+++ aula1208/script.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome: "Cecília",
+    nota:"10",
+    matricula: 16062009
+}
+if(aluno.nota >= 6){
+    alert("APROVADO.")
+}else if(aluno.nota >=4){
+    alert("RECUPERAÇÃO FINAL.")
+}else{
+    alert("REPROVADO.")
+}
